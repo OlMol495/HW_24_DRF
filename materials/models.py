f354ca2089c944a299deb75e77589a41be5e17be
Olga Lebedeva <olga.s.lebedeva@outlook.com>
2024-03-07 15:42:31 +0800

    Initial commit

---
+++ materials/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+from users.models import NULLABLE
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=250, verbose_name='название курса')
+    description = models.TextField(**NULLABLE, verbose_name='описание курса')
+    image = models.ImageField(upload_to='course_images/', **NULLABLE, verbose_name='превью курса')
+
+    def __str__(self):
+        return f"course {self.title}"
+
+    class Meta:
+        verbose_name = 'курс'
+        verbose_name_plural = 'курсы'
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=250, verbose_name='название урока')
+    description = models.TextField(**NULLABLE, verbose_name='описание урокаа')
+    image = models.ImageField(upload_to='course_images/', **NULLABLE, verbose_name='превью урока')
+    video_link = models.URLField(**NULLABLE, verbose_name='ссылка на видео')
+    course = models.ForeignKey(Course, **NULLABLE, on_delete=models.SET_NULL, verbose_name='название курса')
+
+    def __str__(self):
+        return f"lesson {self.title}"
+
+    class Meta:
+        verbose_name = 'урок'
+        verbose_name_plural = 'уроки'

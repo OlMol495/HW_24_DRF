57564d1b83f1a4de54aa329582434c910188f16a
Olga Lebedeva <olga.s.lebedeva@outlook.com>
2024-03-18 21:47:07 +0800

    extra task for 25.1, edited get_serializer method for UserViewSet

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, generics, status
+from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 
 from users.models import User, Payment
@@ -10,14 +11,19 @@ from rest_framework.filters import OrderingFilter
 class UserViewSet(viewsets.ModelViewSet):
     """Вывод данных по юзерам с кастомизированной вьюшкой на детали юзера"""
     queryset = User.objects.all()
-    default_serializer = UserSerializer  # Дофолтный сериалайзер
+    default_serializer = UserSerializer  # Дефолтный сериалайзер
     serializers = {
         'retrieve': UserProfileSerializer  # Детали юзера выводятся по UserProfileSerializer
     }
 
     def get_serializer_class(self):
         """Return appropriate serializer"""
-        return self.serializers.get(self.action, self.default_serializer)
+        if self.action == 'retrieve':
+            print(self.get_object())
+            if self.request.user == self.get_object():
+                return UserProfileSerializer
+            return UserPublicProfileSerializer
+        return self.default_serializer
 
 
 class PaymentListAPIView(generics.ListAPIView):
@@ -30,20 +36,20 @@ class PaymentListAPIView(generics.ListAPIView):
     permission_classes = [IsAuthenticated]
 
 
-class UserProfileAPIView(generics.RetrieveAPIView):
-    #serializer_class = UserProfileSerializer
-    queryset = User.objects.all()
-    permission_classes = [IsAuthenticated]
-
-    def get_object(self):
-        user = super().get_object()
-        user.payments = user.payment_set.all()
-        return user
-
-    def get_serializer_class(self):
-        if self.request.user == self.get_object():
-            return UserProfileSerializer
-        return UserPublicProfileSerializer
+# class UserProfileAPIView(generics.RetrieveAPIView):
+#     #serializer_class = UserProfileSerializer
+#     queryset = User.objects.all()
+#     permission_classes = [IsAuthenticated]
+#
+#     def get_object(self):
+#         user = super().get_object()
+#         user.payments = user.payment_set.all()
+#         return user
+#
+#     def get_serializer_class(self):
+#         if self.request.user == self.get_object():
+#             return UserProfileSerializer
+#         return UserPublicProfileSerializer
 
 
 

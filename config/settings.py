de7855cac23715a4ae32afa6965b70323d6bfc50
Olga Lebedeva <olga.s.lebedeva@outlook.com>
2024-04-12 20:05:32 +0300

    edited .env.sample

--- config/settings.py
+++ config/settings.py
@@ -92,9 +92,9 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "hw24drf",
-        "USER": "postgres",
-        "PASSWORD": "5758",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PW"),
         "HOST": "127.0.0.1",
         "PORT": "5432"
     }
